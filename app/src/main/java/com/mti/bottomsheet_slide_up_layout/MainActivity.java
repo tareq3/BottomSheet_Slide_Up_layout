abd011b2ca7e1877c1be1bbb0e75188cb50b968b
Tareq3 <mti.tareq3@gmail.com>
2018-05-29 02:32:54 +0600

    Initial commit

---
+++ app/src/main/java/com/mti/bottomsheet_slide_up_layout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mti.bottomsheet_slide_up_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
